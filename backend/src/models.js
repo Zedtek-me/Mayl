208ba9f9d2d50ca42d4db65afc7931ce3102ae51
Zechariah <zechariahadebayo42@gmail.com>
2023-12-16 00:56:31 +0100

    creating models; setting prisma

--- backend/src/models.js
+++ backend/src/models.js
@@ -2,8 +2,12 @@ const {PrismaClient} = require("@prisma/client")
 
 const prisma = new PrismaClient()
 const UserModel = prisma.User
+const EmailAccounts = prisma.EmailAccounts
+const EmailMessages = prisma.EmailMessages
 
 
 module.exports = {
-    UserModel
+    UserModel,
+    EmailAccounts,
+    EmailMessages
 }
