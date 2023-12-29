1ea3828fa3b6f259265913a866650cb64a4bdefe
Zechariah <zechariahadebayo42@gmail.com>
2023-12-29 23:24:47 +0100

    auth implementation for the login page--done

--- frontend/src/utils/authUtils.js
+++ frontend/src/utils/authUtils.js
@@ -1,7 +1,8 @@
-export const signupAndSignin = (e, setter)=>{
+export const updateSignupAndSignin = (e, setter)=>{
     let element = e.target
     let key = element.name
     let value = element.value
+    console.log(key, value)
     let updated = new Object()
     updated[key] = value
     setter((prevState)=>{
@@ -10,16 +11,24 @@ export const signupAndSignin = (e, setter)=>{
 }
 
 
-export const fetchUser = async (userProps)=>{
+export const fetchUser = async (e, stateProps)=>{
     /**returns a user object from the backend */
+    e.preventDefault()
     let response = await fetch("http://localhost:7000/user", {
         method:"POST",
-        body: JSON.stringify(userProps),
+        body: JSON.stringify(stateProps),
         headers:{
             "Content-Type":"application/json",
-            "Token":""
         }
     })
     let data = response.json()
     return data.errors || data
+}
+
+export const routeUserBasedOnFetchResult = async (e, stateProps, navigator)=>{
+    let data = await fetchUser(e, stateProps)
+    if(!data.user){
+        return //show a toast notification on the frontend
+    }
+    return navigator("/dashboard")
 }
