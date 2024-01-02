b06ca4466ea40506fc372ffa1f8124ab7b559696
Zechariah <zechariahadebayo42@gmail.com>
2024-01-02 23:45:54 +0100

    bootstrapping utils for email sending; creating composer component

---
+++ frontend/src/components/HomeComponents/Dashboard/ComposeMail.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react"
+import "../../../styles/HomeStyles/Home.css"
+
+
+const ComposeMail = ()=>{
+    return (
+        <div>
+            compose mail section
+        </div>
+    )
+}
+
+
+export default ComposeMail
